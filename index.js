0adf2611cf770fe2be0420c156987de47eeafe86
Ng Jih Yann <devilng05108@Ngs-MacBook-Pro.local>
2019-08-23 12:18:21 +0800

    first version

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+//
+// AppRegistry.registerComponent(appName, () => App);
+
+
+import { AppRegistry } from 'react-native';
+import Root from './src/index';
+
+AppRegistry.registerComponent('imagecarousel', () => Root);
